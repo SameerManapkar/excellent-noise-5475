6d3113965d1e2b24ec5216a154e05c9f7ab948c6
murad2243 <muradkodalwad360@gmail.com>
2023-01-18 22:10:47 +0530

    day2

---
+++ js/navbar.js
@@ -0,0 +1,53 @@
+    function leaves(){
+        pantsInfo.style.visibility = "hidden"
+        pantsInfo.style.height = "0px"
+        suit.style.visibility = "hidden"
+        suit.style.height = "0px"
+        shirtsInfo.style.visibility = "hidden"
+       shirtsInfo.style.height = "0px"
+       sweatersInfo.style.visibility = "hidden"
+       sweatersInfo.style.height = "0px"
+       jacketsInfo.style.visibility = "hidden"
+       jacketsInfo.style.height = "0px"
+       shoesInfo.style.visibility = "hidden"
+       shoesInfo.style.height = "0px"
+       accsInfo.style.visibility="hidden"
+       accsInfo.style.height="0px"
+    }
+    let accsInfo = document.getElementById("accsInfo");
+    let shoesInfo = document.getElementById("shoesInfo");
+    let jacketsInfo = document.getElementById("jacketsInfo");
+     let suit = document.getElementById("suitsInfo");
+    let shirtsInfo = document.getElementById("shirtsInfo");
+    let pantsInfo =document.getElementById("pantsInfo");
+    let sweatersInfo = document.getElementById("sweatersInfo")
+    function bigImg(){
+        shirtsInfo.style.visibility = "visible"
+        shirtsInfo.style.height="auto"
+    }
+    function suits(){
+        suit.style.height="auto"
+        suit.style.visibility = "visible"
+    }
+    function pantInfo(){
+       pantsInfo.style.visibility="visible"
+       pantsInfo.style.height="auto"
+
+    }
+    function sweaterInfo(){
+        sweatersInfo.style.visibility = "visible"
+        sweatersInfo.style.height="auto"
+
+    }
+    function jacketInfo(){
+        jacketsInfo.style.height="auto"
+        jacketsInfo.style.visibility ="visible"
+    }
+    function shoeInfo(){
+        shoesInfo.style.height="auto"
+        shoesInfo.style.visibility="visible"
+    }
+    function accInfo(){
+        accsInfo.style.height="auto"
+        accsInfo.style.visibility="visible"
+    }
